3ebe2327ee85ae28a2551eba7712efb87cf820df
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-08-30 21:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function hellocode()
+{
+let divelement = document. createElement("div");
+document.body.appendChild(divelement);
+let ptag = document. createElement("p");
+let a = navigator.userAgent;
+ptag.innerText = "You are using " + navigator.appName +" "+"version" + navigator.appVersion
+divelement.appendChild(ptag);
+	// alert("dgvsfdgbfdsbfdsb")
+}
