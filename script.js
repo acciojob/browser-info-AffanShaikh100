7072c5ccfbc016851b5c6b34a8b825150cdb1995
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-08-30 21:47:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
 function hellocode()
 {
-let divelement = document. createElement("div");
-document.body.appendChild(divelement);
+let divelement = document.getElementById("browser-info");
+	console.log(divelement);
+// document.body.appendChild(divelement);
 let ptag = document. createElement("p");
-let a = navigator.userAgent;
+// let a = navigator.userAgent;
 ptag.innerText = "You are using " + navigator.appName +" "+"version" + navigator.appVersion
 divelement.appendChild(ptag);
 	// alert("dgvsfdgbfdsbfdsb")
